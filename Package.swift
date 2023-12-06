233f6148f456f6fce749e82eed0a78797701ddee
manibaratam94 <134288910+manibaratam94@users.noreply.github.com>
2023-12-06 15:31:16 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VoiceActivityDetector",
+    products: [
+        .library(name: "VoiceActivityDetector", targets: ["VoiceActivityDetector"])
+    ],
+    targets: [
+        .target(name: "VoiceActivityDetector", path: "Source", publicHeadersPath: ""),
+    ],
+    swiftLanguageVersions: [.v5]
+)
